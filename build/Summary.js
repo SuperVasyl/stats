da32083e0749a81a4512b30cb357a3b29e23eac2
Vasyl Doroshenko <supervasyl10@gmail.com>
2022-01-30 11:20:31 +0200

    added changes to MatchReader.ts and Summary.ts

--- build/Summary.js
+++ build/Summary.js
@@ -1,10 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Summary = void 0;
+const WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
+const HtmlReport_1 = require("./reportTargets/HtmlReport");
 class Summary {
     constructor(anylizer, outputTarget) {
         this.anylizer = anylizer;
         this.outputTarget = outputTarget;
+    }
+    static winsAnalysisWithHtmlReport(team) {
+        return new Summary(new WinsAnalysis_1.WinsAnalysis(team), new HtmlReport_1.HtmlReport());
     }
     buildAndPrintReport(matches) {
         const output = this.anylizer.run(matches);
--- build/index.js
+++ build/index.js
@@ -1,15 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const MatchReader_1 = require("./MatchReader");
-const CsvFileReader_1 = require("./CsvFileReader");
-const WinsAnalysis_1 = require("./analyzers/WinsAnalysis");
 const Summary_1 = require("./Summary");
-const HtmlReport_1 = require("./reportTargets/HtmlReport");
 // Create an object that implements the DataReader interface
-const csvFileReader = new CsvFileReader_1.CsvFileReader('football.csv');
+// const csvFileReader = new CsvFileReader('football.csv');
 // Create an object of MatchReader and pass DataReader implementation
-const matchReader = new MatchReader_1.MatchReader(csvFileReader);
+// const matchReader = new MatchReader(csvFileReader);
+const matchReader = MatchReader_1.MatchReader.fromCsv('football.csv');
 matchReader.load();
 // const summary = new Summary(new WinsAnalysis('Man United'), new ConsoleReport());
-const summary = new Summary_1.Summary(new WinsAnalysis_1.WinsAnalysis('Man United'), new HtmlReport_1.HtmlReport());
-summary.buildAndPrintReport(matchReader.matches);
+// const summary = new Summary(new WinsAnalysis('Man United'), new HtmlReport());
+const summary1 = Summary_1.Summary.winsAnalysisWithHtmlReport('Man United');
+summary1.buildAndPrintReport(matchReader.matches);
